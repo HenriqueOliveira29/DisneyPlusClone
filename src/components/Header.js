be79f0583b4db8a75a3328ae4d30d4483086ee09
Henrique Oliveira <henrique.olive29@gmail.com>
2021-11-23 18:34:18 +0000

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,6 +31,7 @@ function Header() {
                     <span>SERIES</span>
                 </a>
             </NavMenu>
+            <UserImg src="https://avatars.githubusercontent.com/u/68897798?v=4" alt="image profile"/>
         </Nav>
     )
 }
@@ -43,6 +44,7 @@ const Nav = styled.nav`
     display: flex;
     align-items:center;
     padding: 0 36px;
+    overflow-x: hidden;
 `
 
 const Logo = styled.img`
@@ -52,18 +54,51 @@ const Logo = styled.img`
 
 const NavMenu = styled.div`
     display: flex;
+    flex:1;
+    margin-left:25px;
+    align-items:center;
 
     a{
         display:flex;
         align-items:center;
         padding: 0 12px;
+        cursor:pointer;
         img{
             height:20px;
         }
         span{
             font-size: 13px;
             letter-spacing: 1.42px;
+            position: relative;
+
+            &:after{
+                content:"";
+                height:2px;
+                background: white;
+                position: absolute;
+                left:0;
+                right:0;
+                bottom:-6px;
+                opacity:0;
+                transform-origin: left center;
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+                transform: scaleX(0);
+            }
+        }
+
+        &:hover{
+            span:after{
+                transform: scaleX(1);
+                opacity:1;
+            }
         }
     }
+
+`
+const UserImg = styled.img`
+    width:48px;
+    height:48px;
+    border-radius: 50%;
+    cursor:pointer;
 
 `
