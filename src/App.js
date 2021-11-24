7eb4296cdd2b4c620233726cda3652ec28b5f861
Henrique Oliveira <henrique.olive29@gmail.com>
2021-11-24 16:45:06 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link
+  
+} from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Home></Home>
+      <Router>
+        <Header></Header>
+          <Switch>
+            <Route path="/detail">
+              <Detail></Detail>
+            </Route>
+            <Route path="/">
+                <Home></Home>
+            </Route>
+          </Switch>
+      </Router>
+      
+      
     </div>
   );
 }
