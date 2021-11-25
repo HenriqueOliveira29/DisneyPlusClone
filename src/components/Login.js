e1c1e58adfbd2438456e15179158cb48aa1734a2
Henrique Oliveira <henrique.olive29@gmail.com>
2021-11-25 10:57:56 +0000

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,7 +4,13 @@ import styled from 'styled-components'
 function Login() {
     return (
         <Container>
-
+            <CTA>
+                <CTALogoOne src="/images/cta-logo-one.svg"/>
+                <SignUp>
+                    GET ALL THERE
+                </SignUp>
+                
+            </CTA>
         </Container>
     )
 }
@@ -14,9 +20,57 @@ export default Login
 const Container = styled.div`
     position: relative;
     height: calc(100vh - 70px);
+    display:flex;
+    align-items: center;
+    justify-content: center;
 
     &:before{
+        background-image: url(/images/login-background.jpg); 
+        background-position: top;
+        background-size: cover;
+        background-repeat: no-repeat;
         position: absolute;
-
+        content: "";
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        
+        z-index: -1;
     }
+`
+
+const CTA = styled.div`
+    max-width: 650px;
+    padding: 80px 40px;
+    width: 70%;
+    display: flex;
+    flex-direction: column;
+    
+    
+`
+
+const CTALogoOne = styled.img`
+
+`
+
+const SignUp = styled.a`
+    width: 100%;
+    background-color: #0063e5;
+    font-weight: bold;
+    padding: 17px 0;
+    color: #f9f9f9;
+    border-radius: 4px;
+    text-align: center;
+    font-size: 18px;
+    cursor:pointer;
+    transition: all 250ms;
+    letter-spacing: 1.5px;
+    margin-top: 8px;
+    margin-bottom: 12px;
+
+    &:hover{
+        background-color: #0483ee;
+    }
+    
 `
