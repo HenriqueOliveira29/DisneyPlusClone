cb2909abcaf9fc7820eb124799704305492bdeea
Henrique Oliveira <henrique.olive29@gmail.com>
2021-11-27 15:21:18 +0000

    update

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,9 @@ import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Viewers from './Viewers'
 import Movies from './Movies'
-import db from '../firebase'
+
 
 function Home() {
-
-    useEffect(()=>{
-        db.collection("movies").onSnapshot((snapshot)=>{
-            let tempMovies = snapshot.docs.map((doc)=>{
-                console.log(doc.data());
-                return{ id: doc.id , ...doc.data()}
-            });
-        });
-    }, [])
-
-
     return (
         <Container>
            <ImgSlider/>
